d23dc7aa02e3fafbc2697fb0cc6bff12db39d9b5
peter pits <thengugi@gmail.com>
2014-05-12 02:25:27 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+
+	<title>peter pits</title>
+	 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css">
+
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap-theme.min.css">
+
+	<!--css themes -->
+	<link rel="stylesheet"  href="//code.jquery.com/ui/1.10.4/smoothness/jquery-ui.css">
+
+	<!--JQuery css-->
+	<script src="//code.jquery.com/jquery-1.10.2.min.js"></script>
+
+	<!--fontawesome-->
+	<link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+
+	<!--UI-->
+	<script src="//code.jquery.com/ui/1.10.4.jquery-ui.js"></script>
+
+	<!--link javascript-->		
+	<script src="js/bootstrap.min.js"></script>
+
+	
+	<style>
+		/* Sticky footer styles
+-------------------------------------------------- */
+
+html,
+body {
+  height: 100%;
+  text-align: center;
+  /* The html and body elements cannot have any padding or margin. */
+}
+
+/* Wrapper for page content to push down footer */
+#wrap
+  min-height: 100%;
+  height: auto;
+  /* Negative indent footer by its height */
+  margin: 0 auto -60px;
+  /* Pad bottom by footer height */
+  padding: 0 0 60px;
+}
+
+/* Set the fixed height of the footer here */
+#footer {
+  height: 60px;
+  background-color: #f5f5f5;
+}
+
+	</style>
+</head>
+<body>
+<div class="navbar-collapse collapse navbar-responsive-collapse">
+
+<ul class="nav nav-tabs" style="margin-bottom: 15px;">
+
+<li class="a<ul class="nav nav-tabs" style="margin-bottom: 15px;"ctive"><a href="index.php">Home</a></li>
+<li><a href="#">Project Manager</a></li>
+<li class="dropdown">
+ <a href="#" class="dropdown-toggle" data-toggle="dropdown">Junior Editor<b class="caret"></b></a>
+ <ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu2">
+ <li><a href="#">CC-Name</a></li>
+ <li><a href="#">State</a></li>
+ <li><a href="#">Date Received</a></li>
+ <li><a href="#">Issue/Topic</a></li>
+ <li><a href="#">Description</a></li>
+ </ul>
+</li>
+
+<li><a href="#">D/R</a></li>
+<li class="dropdown">
+
+<a href="#" class="dropdown-toggle" data-toggle="dropdown">View Story by<b class="caret"></b></a>
+ <ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu2">
+ 	<li><a href="#">CC-Name</a></li>
+ 	<li><a href="">State</a></li>
+ 	<li><a href="#">Issue</a></li>
+ 	</ul>
+</li>
+
+<li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Stage<b class="caret"></b></a>
+          <ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu2">
+            <li><a href="#">Junior Editor</a></li>
+            <li><a href="#">Footage Check  <i class="fa fa-video-camera"></i></a></li>
+            <li><a href="#">Payment Status  <i class="fa fa-inr"></i></a></li>
+            <li><a href="#">Publish  <i class="fa fa-print"></i> </a></li>
+            <li><a href="#">Senior Editor</a></li>
+          </ul>
+        </li>
+<li><a href="#">New Story  <i class="fa fa-pencil"></i></a></li>
+</ul>
+</div>
+
+</body>
+</html>
+  
